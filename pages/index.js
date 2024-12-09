2f54a05a4c8afd244e4c18d7cb62675e25a1b4d7
Leonardo Penz <59891933+leonardopenz@users.noreply.github.com>
2024-12-09 22:08:54 +0000

    Mensagem nova de Desenvolvimento

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Teste 28/11/2024</h1>;
+  return (
+    <div className="container">
+      <h1>Estamos Trabalhando!</h1>
+      <p>Nossa página está em construção. Volte em breve!</p>
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default Home;
