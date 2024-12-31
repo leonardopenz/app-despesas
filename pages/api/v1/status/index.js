6d6a0d40025965c0077a063ae668abafc0402214
Leonardo Penz <59891933+leonardopenz@users.noreply.github.com>
2024-12-31 21:04:49 +0000

    renomeado o arquvio `.env` para `.env.development`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,7 +2,7 @@ import database from "../../../../infra/database.js";
 
 async function status(req, res) {
   const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  console.log(result);
   res.status(200).json({ chave: "Página de Status" });
 }
 
